e15d87c1ad7117eab84c87092766ac13c38527f8
Jaedson Silva <contacanal798@gmail.com>
2022-06-26 20:41:36 -0300

    adding "create_item" method to create items in database

--- cookiedb/database/cookiedb.py
+++ cookiedb/database/cookiedb.py
@@ -1,6 +1,8 @@
 from json_handler import JSONHandler
 from exceptions import DatabaseNotFoundError
 
+from typing import Union, Any
+
 
 class CookieDB:
     def __init__(
@@ -10,7 +12,7 @@ class CookieDB:
     ):
         self._json_handler = None
         self._open_database = None
-        self._temp_items = None
+        self._temp_items = {}
 
         if not database_local:
             database_local = './'
@@ -36,3 +38,13 @@ class CookieDB:
 
         self._json_handler.update_database(self._open_database, self._temp_items)
         return False
+
+    def create_item(self, path: Union[str, int], value: Any) -> None:
+        items = self._temp_items
+        path_split = path.split('/')
+
+        for c, i in enumerate(path_split):
+            if c == (len(path_split) - 1):
+                items = items.setdefault(i, value)
+            else:
+                items = items.setdefault(i, {})
