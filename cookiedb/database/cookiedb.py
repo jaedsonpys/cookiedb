d13bd4c87cd8ec0574e10ff329e1d2c406b25ae5
Jaedson Silva <contacanal798@gmail.com>
2022-06-26 18:32:07 -0300

    adding "database_local" parameter and attribute

--- cookiedb/database/cookiedb.py
+++ cookiedb/database/cookiedb.py
@@ -2,11 +2,20 @@ from json_handler import JSONHandler
 
 
 class CookieDB:
-    def __init__(self, key: str = None):
+    def __init__(
+        self,
+        key: str = None,
+        database_local: str = None
+    ):
+        self._json_handler = None
         self._open_database = None
         self._temp_database = None
+
+        if not database_local:
+            database_local = './'
 
         if not key:
             key = 't45tc90GyT4f4Qim0xt3BsSsZ5oEEgPbM9VstlGwfdg='
 
         self._key = key
+        self._database_local = database_local
