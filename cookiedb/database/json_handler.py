717b62c5a042b5c509ee920cd1fc0bf421ad0e46
Jaedson Silva <contacanal798@gmail.com>
2022-06-26 17:01:12 -0300

    creating JSONHandler._save_file method to save files

--- cookiedb/database/json_handler.py
+++ cookiedb/database/json_handler.py
@@ -9,6 +9,11 @@ class JSONHandler:
     def __init__(self, enc_key: str):
         self._fernet = Fernet(enc_key)
 
+    @staticmethod
+    def _save_file(file_content: str, local: str) -> None:
+        with open(local, 'r') as writer:
+            writer.write(file_content)
+
     def encrypt_json(self, obj: dict) -> str:
         dict_str = json.dumps(obj)
         encrypted_dict = self._fernet.encrypt(dict_str.encode())
