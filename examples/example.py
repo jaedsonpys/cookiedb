0698f8e2166bb2b2988d8d9cb5047a82242381dc
Jaedson Silva <contacanal798@gmail.com>
2022-06-26 21:34:04 -0300

    creating simple example of use in examples/

---
+++ examples/example.py
@@ -0,0 +1,33 @@
+from cookiedb import CookieDB
+
+database = CookieDB(database_local='../cookiedb/databases-test')
+database.create_database('MyDatabase', if_not_exists=True)
+database.open('MyDatabase')
+
+database.create_item('languages', {
+    'python': {
+        'name': 'Python',
+        'ext': '.py'
+    },
+    'javascript': {
+        'name': 'JavaScript',
+        'ext': '.js'
+    },
+    'cpp': {
+        'name': 'C++',
+        'ext': '.cpp'
+    }
+})
+
+# commit database changes
+database.commit()
+
+# get items
+languages = database.get_item('languages')
+print(f'All languages: {languages}')
+
+python_lang = database.get_item('languages/python')
+print(f'Python language: {python_lang}')
+
+javascript_ext = database.get_item('languages/javascript/ext')
+print(f'JavaScript file extension: {javascript_ext}')
