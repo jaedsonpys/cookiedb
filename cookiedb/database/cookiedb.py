f69bd89dea7a80f4ca2ac34bf858586a45cb0b96
Jaedson Silva <contacanal798@gmail.com>
2022-06-26 21:53:27 -0300

    creating docstring to all methods in CookieDB class

--- cookiedb/database/cookiedb.py
+++ cookiedb/database/cookiedb.py
@@ -11,6 +11,20 @@ class CookieDB:
         database_local: str = None,
         autocommit: bool = False
     ):
+        """
+        Initializes the **JSONHandler** class and prepares the
+        encryption key.
+
+        All changes are saved in a temporary dictionary until
+        a commit is made, so if `autocommit` is not enabled,
+        use the `CookieDB.commit()` method.
+
+        :param key: Encryption key;
+        :param database_local: Database directory;
+        :param autocommit: If "True", changes will be saved every
+        time a database method is called.
+        """
+
         self._json_handler = None
         self._open_database = None
         self._temp_items = {}
@@ -32,6 +46,15 @@ class CookieDB:
             self.commit()
 
     def open(self, database_name: str) -> None:
+        """
+        Stores the name of the database if it exists,
+        otherwise an exception `DatabaseNotFoundError`
+        is thrown.
+
+        :param database_name: Database name;
+        :return: None.
+        """
+
         database = self._json_handler.exists_database(database_name)
 
         if not database:
@@ -39,7 +62,19 @@ class CookieDB:
         else:
             self._open_database = database_name
 
-    def create_database(self, database_name, if_not_exists: bool = False):
+    def create_database(self, database_name, if_not_exists: bool = False) -> None:
+        """
+        Create a database at the location specified
+        in **database local** in the `CookieDB`
+        class instance.
+
+        :param database_name: Database name;
+        :param if_not_exists: If "True", exceptions will
+        not be thrown if you are trying to create a
+        database that already exists;
+        :return: None.
+        """
+
         if not self._json_handler.exists_database(database_name):
             self._json_handler.create_json_database(database_name)
         else:
@@ -47,6 +82,12 @@ class CookieDB:
                 raise DatabaseExistsError(f'Database {database_name} already exists.')
 
     def commit(self) -> bool:
+        """
+        Save changes made to the database.
+
+        :return: Returns "True" if there were changes to commit.
+        """
+
         if self._temp_items is None:
             return False
 
@@ -54,6 +95,18 @@ class CookieDB:
         return False
 
     def create_item(self, path: Union[str, int], value: Any) -> None:
+        """
+        Creates an item in the database.
+        Each path separated by "/" is a key in the JSON file.
+
+        Values can be of type `str`, `int`,
+        `float`, `dict`, `list`, or `tuple`.
+
+        :param path: Item path;
+        :param value: Item value;
+        :return: None.
+        """
+
         items = self._temp_items
         path_split = path.split('/')
 
@@ -66,6 +119,14 @@ class CookieDB:
         self._auto_commit()
 
     def get_item(self, path: Union[str, int]) -> Any:
+        """
+        Get a database item from the path.
+
+        :param path: Item path;
+        :return: Returns the obtained value.
+        None if nothing is found.
+        """
+
         path_split = path.split('/')
         item = {}
 
