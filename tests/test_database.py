964465d7a1fdb330029fa8f1d09de5c30027c2d1
Jaedson Silva <contacanal798@gmail.com>
2022-06-27 14:47:19 -0300

    creating test "test_get_items_1" in TestDatabase

--- tests/test_database.py
+++ tests/test_database.py
@@ -71,6 +71,13 @@ class TestDatabase(SeqTest):
         self.cookiedb.create_item('languages/programming', self.programming_languages)
         self.cookiedb.create_item('languages/markup', self.markup_languages)
 
+    def test_get_items_1(self):
+        languages_db = self.cookiedb.get_item('languages/programming')
+        markup_languages = self.cookiedb.get_item('languages/markup')
+
+        self.is_true(languages_db == self.programming_languages, msg_error='"languages/programming" not equal values')
+        self.is_true(markup_languages == self.markup_languages, msg_error='"languages/markup" not equal values')
+
 
 if __name__ == '__main__':
     TestDatabase().run()
