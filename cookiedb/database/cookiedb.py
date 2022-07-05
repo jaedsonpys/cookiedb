3b51b7411bebcc1b5fd10431542f47ac4eda958f
Jaedson Silva <contacanal798@gmail.com>
2022-07-05 01:06:32 -0300

    adding method to delete items from database

--- cookiedb/database/cookiedb.py
+++ cookiedb/database/cookiedb.py
@@ -182,3 +182,24 @@ class CookieDB:
                     last_items = sub_item
 
         return last_items
+
+    def delete(self, path: str) -> bool:
+        database = self._json_handler.get_database(self._open_database)
+        database_items = database.get('items')
+
+        df = database_items
+
+        path_list = path.split('/')
+        path_list_filtered = []
+
+        for i in path_list:
+            if i != '':
+                path_list_filtered.append(i)
+
+        for c, i in enumerate(path_list_filtered):
+            if c == (len(path_list_filtered) - 1):
+                df = df.pop(i)
+            else:
+                df = df.setdefault(i, {})
+
+        self._json_handler.update_database(self._open_database, database_items)
