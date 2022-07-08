1ea5b7bb58370105426d29308c572fb2520bd756
Jaedson Silva <contacanal798@gmail.com>
2022-07-08 17:04:55 -0300

    update examples/example.py

--- examples/example.py
+++ examples/example.py
@@ -4,7 +4,7 @@ database = CookieDB()
 database.create_database('MyDatabase', if_not_exists=True)
 database.open('MyDatabase')
 
-database.create_item('languages', {
+database.add('languages', {
     'python': {
         'name': 'Python',
         'ext': '.py'
@@ -23,10 +23,10 @@ database.create_item('languages', {
 database.commit()
 
 # get items
-languages = database.get_item('languages')
+languages = database.get('languages')
 print(f'All languages: {languages}')
 
-python_lang = database.get_item('languages/python')
+python_lang = database.get('languages/python')
 print(f'Python language: {python_lang}')
 
 javascript_ext = database.get_item('languages/javascript/ext')
