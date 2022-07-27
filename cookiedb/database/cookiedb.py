82f6acaf699b9ea1abdc344f03f7a98cb0a72029
Jaedson Silva <contacanal798@gmail.com>
2022-07-27 14:53:37 -0300

    using _filter_path_list method in CookieDB.delete

--- cookiedb/database/cookiedb.py
+++ cookiedb/database/cookiedb.py
@@ -166,15 +166,10 @@ class CookieDB:
         database_items = self._get_database_items()
         df = database_items
 
-        path_list = path.split('/')
-        path_list_filtered = []
+        path_list = self._filter_path_list(path.split('/'))
 
-        for i in path_list:
-            if i != '':
-                path_list_filtered.append(i)
-
-        for c, i in enumerate(path_list_filtered):
-            if c == (len(path_list_filtered) - 1):
+        for c, i in enumerate(path_list):
+            if c == (len(path_list) - 1):
                 df = df.pop(i)
             else:
                 df = df.setdefault(i, {})
