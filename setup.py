20e8592a447cdbd54465f0c6f263f3cc5854efa0
Jaedson Silva <contacanal798@gmail.com>
2022-11-18 19:50:45 -0300

    update project version to 5.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='imunknowuser@protonmail.com',
     name='cookiedb',
     description='CookieDB is a noSQL document database.',
-    version='4.2.7',
+    version='5.0.0',
     long_description_content_type='text/markdown',
     long_description=readme,
     license='GPL',
