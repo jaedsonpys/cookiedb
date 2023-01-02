9820a7a7325269f774bd73fe294117b6642121a7
Jaedson Silva <contacanal798@gmail.com>
2023-01-02 10:36:42 -0300

    remove json module import

--- cookiedb/_document.py
+++ cookiedb/_document.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import json
 import os
 from datetime import datetime
 from typing import Union
