e3f231fd2c8d4dcc3cdc0518359622911fadaec7
Jaedson Silva <contacanal798@gmail.com>
2023-02-10 17:35:40 -0300

    feat: add _get_hmac() to generate mac from data

--- cookiedb/_encrypt.py
+++ cookiedb/_encrypt.py
@@ -1,4 +1,9 @@
 import hashlib
+import secrets
+
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
+from Crypto.Hash import HMAC, SHA256
 
 
 class Cryptography:
@@ -6,3 +11,8 @@ class Cryptography:
         hash_key = hashlib.sha256(key.encode()).digest()
         self._encryption_key = hash_key[:128]
         self._signature_key = hash_key[128:]
+
+    def _get_hmac(self, data: bytes) -> bytes:
+        hmac = HMAC.new(self._signature_key, digestmod=SHA256)
+        hmac.update(data)
+        return hmac.digest()
