9526371fce832100c65a077cee29f92cde256051
Jaedson Silva <contacanal798@gmail.com>
2023-02-11 13:47:38 -0300

    build: update project documentation URL

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url='https://github.com/jaedsonpys/cookiedb',
     project_urls={
         'License': 'https://github.com/jaedsonpys/cookiedb/blob/master/LICENSE',
-        'Documentation': 'https://github.com/jaedsonpys/cookiedb/tree/master/DOCS'
+        'Documentation': 'https://jaedsonpys.github.io/cookiedb/'
     },
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
@@ -30,5 +30,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Database :: Database Engines/Servers'
     ],
-    keywords=['database', 'noSQL', 'document', 'JSON', 'sql']
+    keywords=['database', 'noSQL', 'document', 'JSON', 'key-value']
 )
