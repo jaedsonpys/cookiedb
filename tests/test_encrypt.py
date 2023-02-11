d457d4591df2af7410f8325e50e0adf224692328
Jaedson Silva <contacanal798@gmail.com>
2023-02-10 23:08:03 -0300

    test: create test_encrypt_data()

--- tests/test_encrypt.py
+++ tests/test_encrypt.py
@@ -13,6 +13,12 @@ class TestEncryption(bupytest.UnitTest):
         self._fake_key = 'my-fake-secret-key'
         self._enc = _encrypt.Cryptography(self._key)
 
+        self._data = b'My name is John'
+
+    def test_encrypt_data(self):
+        encrypted = self._enc.encrypt(self._data)
+        self.assert_true(encrypted != self._data)
+
 
 if __name__ == '__main__':
     bupytest.this()
