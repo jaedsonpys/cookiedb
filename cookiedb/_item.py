18f9d07a0008cc93479c4b10ad8d9b2becb79940
Jaedson Silva <jaedson.dev@proton.me>
2023-07-10 11:15:58 -0300

    refactor: return only path in get_path() method

--- cookiedb/_item.py
+++ cookiedb/_item.py
@@ -27,10 +27,10 @@ class Item:
         _packv = (path_len, path.encode(), value_len, value_type, value)
         return struct.pack(f'<H{path_len}s HH{value_format}', *_packv)
 
-    def get_path(self) -> Tuple[int, str]:
+    def get_path(self) -> str:
         path_len, = struct.unpack('<H', self._item_io.read(2))
         path, = struct.unpack(f'<{path_len}s', self._item_io.read(path_len))
-        return path_len, path
+        return path
 
     def get_value(self) -> Union[int, float, str]:
         value_len, value_type = struct.unpack('<HH', self._item_io.read(4))
