d1b4b595b711e04300578fadd0540aa19647fb74
Jaedson Silva <jaedson.dev@proton.me>
2023-07-10 11:03:58 -0300

    feat: create method to decode value from item

--- cookiedb/_document.py
+++ cookiedb/_document.py
@@ -49,3 +49,21 @@ class Document:
         path_len, = struct.unpack('<H', item_io.read(2))
         path, = struct.unpack(f'<{path_len}s', item_io.read(path_len))
         return path_len, path
+
+    def _decode_value(path_len: int, item: bytes) -> Union[int, float, str]:
+        item_io = BytesIO(item)
+        path_header_len = path_len + 2
+        item_io.seek(path_header_len)
+
+        value_len, value_type = struct.unpack('<HH', item_io.read(4))
+        value_buffer = item_io.read(value_len)
+
+        if value_type == 1:
+            value, = struct.unpack(f'<{value_len}s', value_buffer)
+            value = value.decode()
+        elif value_type == 2:
+            value, = struct.unpack(f'<i', value_buffer)
+        elif value_type == 3:
+            value, = struct.unpack(f'<f', value_buffer)
+
+        return value
