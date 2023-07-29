c10bf04161162465f1ced3ea0644b4c192eb93ba
Jaedson Silva <jaedson.dev@proton.me>
2023-07-29 14:06:49 -0300

    fix: pass path and list length to _get_list() method

--- cookiedb/_document.py
+++ cookiedb/_document.py
@@ -133,7 +133,7 @@ class Document:
             if item_path == path:
                 return item.get_value()
             elif item_path.startswith(b'@list:' + path):
-                return self._get_list()
+                return self._get_list(path, item.get_value())
             elif item_path.startswith(path):
                 sub_path = item_path.replace(path, b'')
                 items.append((sub_path.decode(), item.get_value()))
